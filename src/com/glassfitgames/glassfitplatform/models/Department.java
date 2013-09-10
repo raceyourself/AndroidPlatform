99864dbf385ddf52557265520d58b366a5310b5e
ben <ben@ben-laptop.(none)>
2013-09-10 10:08:38 +0100

    BL: GlassFitPlatform with basic ORM, no GPS yet

---
+++ src/com/glassfitgames/glassfitplatform/models/Department.java
@@ -0,0 +1,29 @@
+package com.glassfitgames.glassfitplatform.models;
+
+import java.util.List;
+
+import com.roscopeco.ormdroid.Entity;
+
+import static com.roscopeco.ormdroid.Query.eql;
+
+//demo model, will be replaced soon
+public class Department extends Entity {
+  public int id;
+  public String name;
+  
+  public Department() {
+    this(null);
+  }
+  
+  public Department(String name) {
+    this.name = name;
+  }
+  
+  public List<Person> people() {
+    return query(Person.class).where(eql("department", id)).executeMulti();
+  }
+  
+  public String toString() {
+    return name;
+  }
+}
---
+++ src/com/glassfitgames/glassfitplatform/models/Person.java
@@ -0,0 +1,29 @@
+package com.glassfitgames.glassfitplatform.models;
+
+import com.roscopeco.ormdroid.Entity;
+
+//demo model, will be replaced soon
+public class Person extends Entity {
+  public int id;
+  public String name;
+  public String telephone;
+  public Department department;
+  
+  public Person() {
+    this(null, null, null);
+  }
+  
+  public Person(String name, String telephone) {
+    this(name, telephone, null);    
+  }
+  
+  public Person(String name, String telephone, Department department) {
+    this.name = name;
+    this.telephone = telephone;
+    this.department = department;
+  }
+  
+  public String toString() {
+    return name + " - " + telephone;
+  }
+}
