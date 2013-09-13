96598d459a5ea9316e683986e372676ac90521ca
Ben <ben.lister@glassfitgames.com>
2013-09-13 10:12:17 +0100

    BL: Added TargetTracker

---
+++ src/com/glassfitgames/glassfitplatform/gpstracker/TargetTracker.java
@@ -0,0 +1,31 @@
+package com.glassfitgames.glassfitplatform.gpstracker;
+
+import com.glassfitgames.glassfitplatform.models.Position;
+
+public class TargetTracker {
+    
+    public TargetTracker(Integer trackId) {
+        // TODO Auto-generated method stub
+    }
+
+    public float getPace() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void startTracking() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void stopTracking() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
