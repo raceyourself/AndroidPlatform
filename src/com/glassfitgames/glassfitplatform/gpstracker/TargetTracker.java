898877822593d6a19805886e51a9a7466da20780
Ben <ben.lister@glassfitgames.com>
2013-09-13 19:50:31 +0100

    BL: Updated targetTracker to return cumulative distance from start of route

--- src/com/glassfitgames/glassfitplatform/gpstracker/TargetTracker.java
+++ src/com/glassfitgames/glassfitplatform/gpstracker/TargetTracker.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.glassfitgames.glassfitplatform.models.Position;
 import com.glassfitgames.glassfitplatform.models.Track;
 import com.roscopeco.ormdroid.Entity;
+import com.roscopeco.ormdroid.Query;
 
 
 public class TargetTracker {
@@ -15,12 +16,14 @@ public class TargetTracker {
     
     private long currentTime = 0;
     private int currentElement = 0;
+    
+    long distance = 0l;
         
 	
     public TargetTracker(int trackId) {
     	do {
-	    	track = Entity.query(Track.class).orderBy("id desc").execute();
-	//    	track = Entity.query(Track.class).where(Query.eql("id", trackId)).execute();
+	    	track = Entity.query(Track.class).orderBy("id asc").execute();
+//	    	track = Entity.query(Track.class).where(Query.eql("id", trackId)).execute();
 	    	if (track == null) throw new IllegalArgumentException("No such track");
 	    	Log.i("TargetTracker", "Track: " + track.getId());
 	    	
@@ -46,40 +49,33 @@ public class TargetTracker {
     }
     
     /**
-     * Calculates travelled distance on track between currentTime and currentTime+elapsedTime
-     * NOTE: Updates internal state (currentTime += elapsedTime)
+     * Calculates travelled distance on track between start and time
+     * NOTE: Updates internal state (distance += elapsed distance since last call)
      * 
-     * @param elapsedTime in milliseconds
+     * @param time in milliseconds
      * @return distance in meters 
      */
-    public long getElapsedDistance(int elapsedTime) {    	
-    	long distance = 0l;
-    	int elapsed = 0;
-    	
-    	Position currentPosition = trackPositions.get(currentElement);
-    	long actualElapsed = elapsedTime + (currentTime - currentPosition.getTimestamp());
-    	
-//    	Log.d("TargetTracker", "Current el: " + currentElement + ", time: " + currentPosition.getTimestamp());
-    	while (elapsed <= actualElapsed && currentElement + 1 < trackPositions.size()) {
-        	Position nextPosition = trackPositions.get(currentElement + 1);
-//        	Log.d("TargetTracker", "Next el: " + (currentElement+1) + ", time: " + nextPosition.getTimestamp());
-        	elapsed += Position.elapsedTimeBetween(currentPosition, nextPosition);
-//        	Log.d("TargetTracker", "Elapsed: " + elapsed);
-        	// Only increment element and add distance if ahead of position
-    		if (elapsed <= actualElapsed) {
-    			distance += Position.distanceBetween(currentPosition, nextPosition);
-        		currentElement++;
-    		}
-    		
-    		// Store current for next iteration
-    		currentPosition = nextPosition;
-    	}
-    	// NOTE: Time keeps running while currentElement is at tail
-    	// TODO: Wrap around?
-    	currentTime += elapsedTime;
-    	
-  //  	Log.i("TargetTracker", "Time: " + currentTime + ", element: " + currentElement);
+    public long getCumulativeDistanceAtTime(long time) {
+
+        Position currentPosition = trackPositions.get(currentElement);
+        if (currentElement + 1 >= trackPositions.size()) return 0;
+        Position nextPosition = trackPositions.get(currentElement + 1);
+
+        // long actualElapsed = elapsedTime + (currentTime -
+        // currentPosition.getTimestamp());
+
+        // Log.d("TargetTracker", "Current el: " + currentElement + ", time: " +
+        // currentPosition.getTimestamp());
+        while (nextPosition.getTimestamp() <= time && currentElement + 1 < trackPositions.size()) {
+            distance += Position.distanceBetween(currentPosition, nextPosition);
+            currentElement++;
+            currentPosition = nextPosition;
+            nextPosition = trackPositions.get(currentElement + 1);
+        }
+        // TODO: Wrap around?
+        // Log.i("TargetTracker", "Element: " + currentElement);
         return distance;
+
     }
     
 }
