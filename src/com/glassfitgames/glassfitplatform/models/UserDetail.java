64e5a04416a8c7aad0583d5050162548a14ba525
husberg <janne@husberg.fi>
2013-09-13 11:31:52 +0100

    Authentication bugfixes.

--- src/com/glassfitgames/glassfitplatform/models/UserDetail.java
+++ src/com/glassfitgames/glassfitplatform/models/UserDetail.java
@@ -1,19 +1,14 @@
 package com.glassfitgames.glassfitplatform.models;
 
-import static com.roscopeco.ormdroid.Query.eql;
-
-import java.sql.Blob;
-import java.util.List;
-
 import com.roscopeco.ormdroid.Entity;
 
 public class UserDetail extends Entity {
 
-	public int user_id; // Auto-generated ID
+	public int id; // Auto-generated ID
 	public String name; // The users full name
 	public int age;// The users age
 	public String email;// The user's email
-	public Blob photo;// Profile photo
+	public String photoUri;// Profile photo URI
 	private String apiAccessToken; //token to access GlassFit server API. May expire. Use auth.GlassFitAccountAuth to set/refresh.
 
 	public UserDetail() {
