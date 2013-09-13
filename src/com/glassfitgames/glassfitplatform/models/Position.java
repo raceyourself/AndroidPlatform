c3ac9707a44efcfab3e6295c5e259a470fc9121b
Ben <ben.lister@glassfitgames.com>
2013-09-13 12:37:30 +0100

    BL: updated Position and GPSTracker

--- src/com/glassfitgames/glassfitplatform/models/Position.java
+++ src/com/glassfitgames/glassfitplatform/models/Position.java
@@ -3,6 +3,8 @@ package com.glassfitgames.glassfitplatform.models;
 import java.sql.Timestamp;
 import java.util.List;
 
+import android.location.Location;
+
 import com.roscopeco.ormdroid.Entity;
 
 import static com.roscopeco.ormdroid.Query.eql;
@@ -13,38 +15,69 @@ public class Position extends Entity {
 	public int position_id; // ideally auto-increment
 	public int track_id;
 	public int state_id;
-	public Timestamp ts;
+	public long ts;
 	public double latx; // Latitude
 	public double lngx; // longitude
-	public float altitude;
+	public double altitude;
 	public float bearing; // which way are we pointing?
 	public float epe; // estimated GPS position error
 	public String nmea; // full GPS NMEA string
-
-	public double getLatx() {
-		return latx;
-	}
-
-	public void setLatx(double latx) {
-		this.latx = latx;
-	}
-
-	public double getLngx() {
-		return lngx;
-	}
-
-	public void setLngx(double lngx) {
-		this.lngx = lngx;
-	}
+	public float speed; // speed in m/s
 
 	public Position() {
-	}
+    }
 
-	public Position(int track_id /* , details from GPS sensor */) {
-		/* set lat, long, altitude etc */
-	}
+    public Position(int trackId, Location location) {
+        track_id = 0;
+        ts = location.getTime();
+        latx = location.getLatitude();
+        lngx = location.getLongitude();
+        if (location.hasAltitude()) altitude = location.getAltitude();
+        if (location.hasBearing()) bearing = location.getBearing();
+        if (location.hasSpeed()) bearing = location.getSpeed();
+    }
+    
+    public double getAltitude() {
+        return altitude;
+    }
 
-	public List<Position> getPositions(int track_id) {
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public float getBearing() {
+        return bearing;
+    }
+
+    public void setBearing(float bearing) {
+        this.bearing = bearing;
+    }
+    
+    public double getLatx() {
+        return latx;
+    }
+
+    public void setLatx(double latx) {
+        this.latx = latx;
+    }
+
+    public double getLngx() {
+        return lngx;
+    }
+
+    public void setLngx(double lngx) {
+        this.lngx = lngx;
+    }
+    
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public List<Position> getPositions(int track_id) {
 		return query(Position.class).where(eql("track_id", track_id))
 				.executeMulti();
 	}
