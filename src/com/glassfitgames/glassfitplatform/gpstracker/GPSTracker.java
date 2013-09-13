19bb1058f779d4b062fbe0ffef5011fcbcc3a9c9
Ben <ben.lister@glassfitgames.com>
2013-09-13 14:11:57 +0100

    BL: updated GPSTracker to log, and to provide skeleton methods for getElapsedTime and getElapsedDistance

--- src/com/glassfitgames/glassfitplatform/gpstracker/GPSTracker.java
+++ src/com/glassfitgames/glassfitplatform/gpstracker/GPSTracker.java
@@ -30,11 +30,13 @@ public class GPSTracker extends Service implements LocationListener, TrackerInte
 
 	// flag for GPS status
 	boolean canGetLocation = false;
+	
+	// flag for whether we're actively tracking
+	boolean isTracking = false;
 
 	Position currentPosition;
 	Location location; // location
-	double latitude; // latitude
-	double longitude; // longitude
+	int trackId; //ID of the current track
 
 	// The minimum distance to change Updates in meters
 	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
@@ -50,78 +52,52 @@ public class GPSTracker extends Service implements LocationListener, TrackerInte
 
 	public GPSTracker(Context context) {
 		this.mContext = context;
+		trackId = 0;
+		initGps();
 	}
 
 	public Position getCurrentPosition() throws Exception {
-		currentPosition = new Position();
-		if (isGPSEnabled) {
-			if (location == null) {
-				locationManager.requestLocationUpdates(
-						LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES,
-						MIN_DISTANCE_CHANGE_FOR_UPDATES,
-						(LocationListener) this);
-				Log.d("GPS Enabled", "GPS Enabled");
-				if (locationManager != null) {
-					location = locationManager
-							.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-					if (location != null) {
-						latitude = location.getLatitude();
-						longitude = location.getLongitude();
-						currentPosition.setLatx(latitude);
-						currentPosition.setLngx(longitude);
-
-					}
-				}
-			} else {
-				latitude = location.getLatitude();
-				longitude = location.getLongitude();
-				currentPosition.setLatx(latitude);
-				currentPosition.setLngx(longitude);
-
-			}
-		}
-		if (currentPosition == null)
-			throw new Exception();
-		return currentPosition;
-	}
-
-	public void initGps() {
-		try {
-			locationManager = (LocationManager) mContext
-					.getSystemService(LOCATION_SERVICE);
-
-			// getting GPS status
-			isGPSEnabled = locationManager
-					.isProviderEnabled(LocationManager.GPS_PROVIDER);
-
-			// getting network status
-			isNetworkEnabled = locationManager
-					.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-			if (!isGPSEnabled && !isNetworkEnabled) {
-				// no network provider is enabled
-			} else {
-				this.canGetLocation = true;
-				if (isNetworkEnabled) {
-					locationManager.requestLocationUpdates(
-							LocationManager.NETWORK_PROVIDER,
-							MIN_TIME_BW_UPDATES,
-							MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-					Log.d("Network", "Network");
-					if (locationManager != null) {
-						location = locationManager
-								.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-						if (location != null) {
-							latitude = location.getLatitude();
-							longitude = location.getLongitude();
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		if (isTracking) {
+		    return currentPosition;
+		} else {
+		    throw new Exception();
 		}
 	}
+
+    public void initGps() {
+        try {
+            locationManager = (LocationManager)mContext.getSystemService(LOCATION_SERVICE);
+
+            // getting GPS status
+            isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+                showSettingsAlert();
+            } else {
+                this.canGetLocation = true;
+                if (isNetworkEnabled) {
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                    Log.d("GlassFitPlatform", "Network position enabled");
+                }
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                                (LocationListener)this);
+                        Log.d("GlassFitPlatform", "GPS Enabled");
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 	public void startTracking() {
         if (canGetLocation) {
@@ -129,6 +105,7 @@ public class GPSTracker extends Service implements LocationListener, TrackerInte
             timer = new Timer();
             task = new GpsTask();
             timer.scheduleAtFixedRate(task, 0, 1000);
+            isTracking = true;
         }
 	}
 
@@ -160,30 +137,6 @@ public class GPSTracker extends Service implements LocationListener, TrackerInte
 		}
 	}
 
-	/**
-	 * Function to get latitude
-	 * */
-	public double getLatitude() {
-		if (location != null) {
-			latitude = location.getLatitude();
-
-		}
-
-		// return latitude
-		return latitude;
-	}
-
-	/**
-	 * Function to get longitude
-	 * */
-	public double getLongitude() {
-		if (location != null) {
-			longitude = location.getLongitude();
-		}
-
-		// return longitude
-		return longitude;
-	}
 
 	/**
 	 * Function to check GPS/wifi enabled
