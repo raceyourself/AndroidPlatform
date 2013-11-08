c3cf1036b185feacb946c35241827a6f5dd82538
Ben <ben.lister@glassfitgames.com>
2013-11-08 15:18:44 +0000

    Added track interpolation/extrapolation methods. Delete a couple of unused exceptions.

--- src/com/glassfitgames/glassfitplatform/models/Position.java
+++ src/com/glassfitgames/glassfitplatform/models/Position.java
@@ -47,7 +47,7 @@ public class Position extends Entity {
 	public String nmea; // full GPS NMEA string
 	public float speed; // speed in m/s
 	@JsonIgnore
-	public double cumulativeDistance; // cumulative distance covered in the track
+	private float invR = (float)0.0000001569612306; // 1/earth's radius (meters)
 
     @JsonIgnore
     public boolean dirty = false;
@@ -163,14 +163,6 @@ public class Position extends Entity {
       this.speed = speed;
   }
 
-	public double getCumulativeDistance() {
-        return cumulativeDistance;
-    }
-    
-    public void setCumulativeDistance(double cumulativeDistance) {
-        this.cumulativeDistance = cumulativeDistance;
-    }
-
     public String toString() {
 		return nmea;
 	}
@@ -192,6 +184,40 @@ public class Position extends Entity {
 	    Location lb = destination.toLocation();    
 	    return la.bearingTo(lb);    
 	}
+	
+	// Precise position prediction based on the last
+    // position, bearing and speed
+    public Position predictPosition(Position aLastPosition, long milliseconds) {
+      System.out.println("predictPosition: Start\n");  
+      System.out.printf("- %f %f, %f m/s, %f\n", 
+                              aLastPosition.getLatx(), aLastPosition.getLngx(), 
+                              aLastPosition.getSpeed(), aLastPosition.getBearing());
+
+       Position next = new Position();
+       float d = aLastPosition.getSpeed() * milliseconds / 1000.0f; // distance = speed(m/s) * time (s)
+
+       float dR = d*invR;
+       // Convert bearing to radians
+       float brng = (float)Math.toRadians(aLastPosition.getBearing());
+       float lat1 = (float)Math.toRadians(aLastPosition.getLatx());
+       float lon1 = (float)Math.toRadians(aLastPosition.getLngx());
+       System.out.printf("d: %f, dR: %f; brng: %f\n", d, dR, brng);
+       // Predict lat/lon
+       float lat2 = (float)Math.asin(Math.sin(lat1)*Math.cos(dR) + 
+                    Math.cos(lat1)*Math.sin(dR)*Math.cos(brng) );
+       float lon2 = lon1 + (float)Math.atan2(Math.sin(brng)*Math.sin(dR)*Math.cos(lat1), 
+                     Math.cos(dR)-Math.sin(lat1)*Math.sin(lat2));
+       // Convert back to degrees
+       next.setLatx((float)Math.toDegrees(lat2));
+       next.setLngx((float)Math.toDegrees(lon2));
+       next.setGpsTimestamp(aLastPosition.getGpsTimestamp() + milliseconds);
+       next.setDeviceTimestamp(aLastPosition.getDeviceTimestamp() + milliseconds);
+       next.setBearing(aLastPosition.getBearing());
+       next.setSpeed(aLastPosition.getSpeed());
+       
+       System.out.printf("predictPosition: End - %f %f\n", next.getLatx(), next.getLngx());  
+       return next;
+    }
 	
 	public Location toLocation() {
         Location l = new Location(LocationManager.GPS_PROVIDER);
--- src/com/glassfitgames/glassfitplatform/models/Track.java
+++ src/com/glassfitgames/glassfitplatform/models/Track.java
@@ -7,6 +7,8 @@ import java.nio.ByteBuffer;
 import java.util.Date;
 import java.util.List;
 
+import android.util.Log;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.roscopeco.ormdroid.Entity;
 
@@ -36,6 +38,20 @@ public class Track extends Entity {
     @JsonIgnore
     public boolean dirty = false;
     public Date deleted_at = null;
+    
+    // internal fields used for iterating through the positions in this track
+    @JsonIgnore
+    private final String LOGTAG = "GlassFitPlatform - Track";
+    @JsonIgnore
+    private List<Position> trackPositions;
+    @JsonIgnore
+    private int currentElement = 0;
+    @JsonIgnore
+    private double distanceAccumulator = 0.0;
+    @JsonIgnore
+    private long trackStartTime;
+    @JsonIgnore
+    private Metric metric;
 
     public Track() {
     }
@@ -108,4 +124,116 @@ public class Track extends Entity {
 		}
 		return super.save();				
 	}
+	
+	public Position getPositionAtTime(long time) {
+	    
+	    // refresh list of track positions from database (may have updated if e.g. currently being recorded)
+	    trackPositions = getTrackPositions();
+	    if (trackPositions.size() == 0) {
+	        Log.e(LOGTAG,"Cannot get position from track " + this.getId() + "because it has no position elements");
+	        return null;
+	    }
+	    
+	    // start from beginning of track if we've switched metrics
+	    if (metric != Metric.TIME) {
+	        currentElement = 0;
+	        distanceAccumulator = 0.0;
+	        trackStartTime = trackPositions.get(0).getDeviceTimestamp();
+	        metric = Metric.TIME;
+	    }
+	    
+	    // iterate through the track until we find the required position
+	    Position p1 = null;
+	    Position p2 = null;
+	    while (true) {
+	        
+	        p1 = trackPositions.get(currentElement);
+	        if (currentElement == trackPositions.size()-1) {
+	            // if we're on the last element, extrapolate
+	            return p1.predictPosition(p1, time);
+	        }
+	        
+	        p2 = trackPositions.get(currentElement+1);
+	        if (p2.getDeviceTimestamp() - trackStartTime > time) {
+	            // if the next element is too far ahead, interpolate
+	            float proportion = (float)(time - p1.getDeviceTimestamp()) / (p2.getDeviceTimestamp() - p1.getDeviceTimestamp());
+	            return interpolate(p1, p2, proportion);
+	        } else {
+	            // increment current position & update cumulative numbers
+	            distanceAccumulator += Position.distanceBetween(p1, p2);
+	            currentElement++;
+	        }
+        }
+	    
+	}
+	
+	
+	public Position getPositionAtDistance(double distance) {
+	    
+	    // refresh list of track positions from database (may have updated if e.g. currently being recorded)
+        trackPositions = getTrackPositions();
+        if (trackPositions.size() == 0) {
+            Log.e(LOGTAG,"Cannot get position from track " + this.getId() + "because it has no position elements");
+            return null;
+        }
+        
+        // start from beginning of track if we've switched metrics
+        if (metric != Metric.DISTANCE) {
+            currentElement = 0;
+            distanceAccumulator = 0.0;
+            metric = Metric.DISTANCE;
+        }
+        
+        // iterate through the track until we find the required position
+        Position p1 = null;
+        Position p2 = null;
+        while (true) {
+            
+            p1 = trackPositions.get(currentElement);
+            if (currentElement == trackPositions.size()-1) {
+                // if we're on the last element, extrapolate
+                return p1.predictPosition(p1, (long)(distance/p1.getSpeed()*1000.0));
+            }
+            
+            p2 = trackPositions.get(currentElement+1);
+            if (distanceAccumulator + Position.distanceBetween(p1,p2) > distance) {
+                // if the next element is too far ahead, interpolate
+                float proportion = (float)((distance-distanceAccumulator) / Position.distanceBetween(p1, p2));
+                return interpolate(p1, p2, proportion);
+            } else {
+                // increment current position & update cumulative numbers
+                distanceAccumulator += Position.distanceBetween(p1, p2);
+                currentElement++;
+            }
+        }
+
+	}
+	
+	
+	private enum Metric {
+        TIME,
+        DISTANCE
+    }
+    
+	// TODO: move to Position.interpolate
+	// TODO: spline rather than linear interpolation
+    private Position interpolate(Position p1, Position p2, float proportion) {
+        
+        double lat = p1.getLatx() + proportion*(p2.getLatx() - p1.getLatx());
+        double lng = p1.getLngx() + proportion*(p2.getLngx() - p1.getLngx());
+        long ts = p1.getDeviceTimestamp() + (long)proportion*(p2.getDeviceTimestamp() - p1.getDeviceTimestamp());
+        float bng = p1.getBearing() + proportion*(p2.getBearing() - p1.getBearing());
+        double dist = distanceAccumulator + proportion*Position.distanceBetween(p1, p2);
+        float speed = p1.getSpeed() + proportion*(p2.getSpeed() - p1.getSpeed());
+        
+        // update current position
+        Position p = new Position();
+        p.setLatx(lat);
+        p.setLngx(lng);
+        p.setDeviceTimestamp(ts);
+        p.setBearing(bng);
+        p.setSpeed(speed);
+        return p;
+    }
+    
 }
