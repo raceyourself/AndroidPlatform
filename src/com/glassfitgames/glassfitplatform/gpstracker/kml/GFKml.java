2b87544810e853c5d0744035aa5d0d77a6f8b572
Alexander Raginsky <alexander.raginsky@gmail.com>
2013-11-16 16:30:01 +0100

    KML writing - paths & styles

--- src/com/glassfitgames/glassfitplatform/gpstracker/kml/GFKml.java
+++ src/com/glassfitgames/glassfitplatform/gpstracker/kml/GFKml.java
@@ -2,6 +2,7 @@ package com.glassfitgames.glassfitplatform.gpstracker.kml;
 
 import java.util.Vector;
 import java.util.List;
+import java.util.ArrayList;
 
 import com.ekito.simpleKML.model.*;
 import com.ekito.simpleKML.Serializer;
@@ -15,11 +16,14 @@ public class GFKml {
     
     // Represents curretly active path. Can be null if no active path
     private Folder path;
+    // Currently active style
+    private String style;
         
     public GFKml() { 
         document.setFeatureList( new Vector<Feature>());
         kml.setFeature(document);
-        // TODO: init styles
+        // Init styles
+        initStyles();
     }
     
     // Start new position's path. Sequential calls to addPosition will add positions
@@ -29,6 +33,7 @@ public class GFKml {
         path = new Folder();
         path.setFeatureList(new Vector<Feature>());
         document.getFeatureList().add(path);
+        // TODO: choose style
     }
     
     // End position path
@@ -41,15 +46,27 @@ public class GFKml {
         if (path == null) {
             return false;
         }
-        String coordsString = Double.toString(pos.getLngx()) + "," + Double.toString(pos.getLatx());
+        // Placemark holds all data about a position
         Placemark pm = new Placemark();
-        Point pt = new Point();
-
-        pt.setCoordinates(new Coordinate(pos.getLngx(), pos.getLatx(), pos.getAltitude()));
+        // Set style
+        pm.setStyleUrl("#" + style);
+        // Add timestamp. TODO: choose between Gps and Device timestamp according to path type
+        TimeStamp ts = new TimeStamp();
+        ts.setWhen(Long.toString(pos.getDeviceTimestamp()));
+        pm.setTimePrimitive(ts);
+        // Geometry list of the placemark
         List<Geometry> lg = new Vector<Geometry>();
-        lg.add(pt);
         pm.setGeometryList(lg);
-        
+        // Add point
+        Point pt = new Point();
+        pt.setCoordinates(positionToCoordinate(pos));
+        lg.add(pt);
+        // Add heading as a line from given to predicted position
+        LineString heading = addHeading(pos);
+        if (heading != null) {
+            lg.add(heading);
+        }
+        // Add placemark to the current path        
         path.getFeatureList().add(pm);
         return true;
 
@@ -58,6 +75,91 @@ public class GFKml {
     public void write(java.io.OutputStream out) throws java.lang.Exception {
         Serializer serializer = new Serializer();
         serializer.write(kml, out);
+    }
+    
+    private Coordinate positionToCoordinate(Position pos) {
+        return new Coordinate(pos.getLngx(), pos.getLatx(), pos.getAltitude());
+    }
+    
+    private String positionToString(Position pos) {
+        return positionToCoordinate(pos).toString();
+    }
+    
+    // Draw line from current to predicted position
+    private LineString addHeading(Position pos) {
+        Position predictedPos = Position.predictPosition(pos, 10); // milliseconds
+        if (predictedPos == null) {
+            return null;
+        }
+        ArrayList<Coordinate> coordList = new ArrayList<Coordinate>();
+        coordList.add(positionToCoordinate(pos));
+        coordList.add(positionToCoordinate(predictedPos));
+
+        Coordinates coords = new Coordinates(positionToString(pos));
+        coords.setList(coordList);
+        
+        LineString ls = new LineString();        
+        ls.setCoordinates(coords);
+        return ls;
+    }
+    
+    private void initStyles() {
+        String color = "ffff0000";        
+        Pair normStylePair = initStyle(color, false);
+        Pair hiliStylePair = initStyle(color, true);
+        normStylePair.getStyle().setId("normalPositionStyleBlue");
+        hiliStylePair.getStyle().setId("hiliPositionStyleBlue");
+        
+        StyleMap styleMap = new StyleMap();
+        style = "generalPositionStyleBlue";
+        styleMap.setId(style);
+        
+        List<StyleSelector> styleSelector = new ArrayList<StyleSelector>();
+        styleSelector.add(normStylePair.getStyle());
+        styleSelector.add(hiliStylePair.getStyle());
+        styleSelector.add(styleMap);
+        
+        document.setStyleSelector(styleSelector);
+    }
+    
+    private Pair initStyle(String color, boolean isHiLi) {
+        String href;
+        float scale;
+        String pairKey;
+        if (isHiLi) {
+           href = "http://maps.google.com/mapfiles/kml/shapes/arrow.png";
+           scale = (float)0.0;
+           pairKey = "highlight";
+           
+        } else {
+           href = "http://maps.google.com/mapfiles/kml/shapes/shaded_dot.png";
+           scale = (float)1.0;
+           pairKey = "normal";
+        }
+
+        // Icon style
+        IconStyle is = new IconStyle();
+        is.setColor(color);
+        Icon ic = new Icon();        
+        ic.setHref(href);
+        is.setIcon(ic);
+        // Label style
+        LabelStyle ls = new LabelStyle();
+        ls.setScale(scale);
+        // Line style
+        LineStyle lis = new LineStyle();
+        is.setColor(color);
+        // Construct style itself
+        Style st = new Style();
+        st.setIconStyle(is);
+        st.setLabelStyle(ls);
+        st.setLineStyle(lis);
+
+        Pair stylePair = new Pair();
+        stylePair.setKey(pairKey);
+        stylePair.setStyle(st);
+        return stylePair;
+        
     }
     
 }
