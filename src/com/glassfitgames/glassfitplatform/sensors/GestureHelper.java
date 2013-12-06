2f2a4447ee2d2f44090c67ead94ec6b8ad657c0d
amerigo14 <amerigo14@hotmail.com>
2013-12-06 13:21:35 +0000

    Updated gestures and added layout

--- src/com/glassfitgames/glassfitplatform/sensors/GestureHelper.java
+++ src/com/glassfitgames/glassfitplatform/sensors/GestureHelper.java
@@ -1,23 +1,116 @@
 package com.glassfitgames.glassfitplatform.sensors;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.widget.FrameLayout;
+import android.widget.FrameLayout.LayoutParams;
 
+import com.glassfitgames.glassfitplatform.R;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
 import com.qualcomm.QCARUnityPlayer.QCARPlayerActivity;
 import com.unity3d.player.UnityPlayer;
 
-public class GestureHelper extends QCARPlayerActivity implements GestureDetector.OnGestureListener {
-	private GestureDetector gestureDetector;
-
+public class GestureHelper extends QCARPlayerActivity {
+	
+	private UnityPlayer mUnityPlayer;
+	
+	GestureDetector mGestureDetector;
+    	
 	@Override
 	 public void onCreate(Bundle savedInstanceState) {
 		 super.onCreate(savedInstanceState);
-		 Log.e("GPSTracker", "Gesture Helper Initiated");
-		 gestureDetector = new GestureDetector(this, this);
+		 
+		 // Create the UnityPlayer
+	        mUnityPlayer = super.getUnityPlayer();
+	        int glesMode = mUnityPlayer.getSettings().getInt("gles_mode", 1);
+	        boolean trueColor8888 = false;
+	        mUnityPlayer.init(glesMode, trueColor8888);
+		 
+		 //gmListener = new GenericMotionListener();
+		 mGestureDetector = createGestureDetector(this);
+		 
+		 setContentView(R.layout.test_layout);
+		 
+		 // Add the Unity view
+	        FrameLayout layout = (FrameLayout) findViewById(R.id.unityLayout);
+	        if(layout == null) {
+	        	Log.e("GestureHelper", "layout null");
+	        }
+	        
+	        if(mUnityPlayer.getView() == null) {
+	        	Log.e("GestureHelper", "Unity view null");
+	        }
+	        LayoutParams lp = new LayoutParams (LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+	        layout.addView(mUnityPlayer.getView(), 0, lp);
+
+			// Set the focus
+	        FrameLayout mainLayout = (FrameLayout) findViewById(R.id.mainLayout);
+			mainLayout.setFocusableInTouchMode(true);
 	 }
 	
+	private GestureDetector createGestureDetector(Context context) {
+		GestureDetector gestureDetector = new GestureDetector(context);
+		
+		gestureDetector.setBaseListener(new GestureDetector.BaseListener() {
+			
+			@Override
+			public boolean onGesture(Gesture gesture) {
+				if(gesture == Gesture.TAP) {
+					Log.e("GestureHelper", "Tap Function On");
+					try {
+						UnityPlayer.UnitySendMessage("Scriptholder", "isTap", "");
+						Log.i("GestureHelper", "Message Sent: Tap");
+					} catch (UnsatisfiedLinkError er) {
+						Log.i("GestureHelper",
+								"Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
+						Log.i("GestureHelper", er.getMessage());
+					}
+					return true;
+				} else if(gesture == Gesture.SWIPE_DOWN) {
+					Log.e("GestureHelper", "Down swipe");
+					return true;
+				} else if(gesture == Gesture.SWIPE_UP) {
+					Log.e("GestureHelper", "Up swipe");
+					return true;
+				}
+				return false;
+			}
+		});
+		gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+              // do something on finger count changes
+            }
+        });
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+                // do something on scrolling
+            	return true;
+            }
+        });
+        return gestureDetector;
+	}
+	
+	@Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+		Log.e("GestureHelper", "Gesture processing");
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
+    }
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		Log.e("GestureHelper", "On touch called");
+		super.onTouchEvent(event);
+		return true;
+	}
+
 	@Override
     protected void onResume() {
         super.onResume();
@@ -26,119 +119,20 @@ public class GestureHelper extends QCARPlayerActivity implements GestureDetector
 	@Override
     protected void onPause() {
         super.onPause();
+ 
 	}
-
-	 @Override
-	 public boolean onGenericMotionEvent(MotionEvent event) {
-		gestureDetector.onTouchEvent(event);
-		return true;
-	}
-
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when the TrackPad is pressed
-	 */
+	
 	@Override
-	public boolean onDown(MotionEvent e) {
-		
-		return true;
+	protected void onDestroy() {
+		Log.e("GestureHelper", "On destroy has been called");
+		super.onDestroy();
+	}
+	
+	@Override
+	protected void onStop() {
+		Log.e("GestureHelper", "On stop called");
+		super.onStop();
 	}
 
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when a fast swipe occurs on the trackpad
-	 */
-	@Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
-			float velocityY) {
-		Log.e("GPSTracker", "Fling Function On");
-		if(velocityX > 3000) {
-			 try {
-		            UnityPlayer.UnitySendMessage("Scriptholder", "flingRight", "");
-		            Log.i("GPSTracker", "Message Sent: Fling Right");
-		        } catch (UnsatisfiedLinkError e) {
-		            Log.i("GPSTracker","Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
-		            Log.i("GPSTracker",e.getMessage());
-		        }
-		}
-			
-		if(velocityX < -3000) {
-			 try {
-		            UnityPlayer.UnitySendMessage("Scriptholder", "flingLeft", "");
-		            Log.i("GPSTracker", "Message Sent: Fling Left");
-		        } catch (UnsatisfiedLinkError e) {
-		            Log.i("GPSTracker","Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
-		            Log.i("GPSTracker",e.getMessage());
-		        }
-		}
-		
-		if(velocityY < -1000) {
-			try {
-	            UnityPlayer.UnitySendMessage("Scriptholder", "flingUp", "");
-	            Log.i("GPSTracker", "Message Sent: Fling Up");
-	        } catch (UnsatisfiedLinkError e) {
-	            Log.i("GPSTracker","Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
-	            Log.i("GPSTracker",e.getMessage());
-	        }
-		}
-		
-		if(velocityY > 1000) {
-			try {
-	            UnityPlayer.UnitySendMessage("Scriptholder", "flingDown", "");
-	            Log.i("GPSTracker", "Message Sent: Fling Down");
-	        } catch (UnsatisfiedLinkError e) {
-	            Log.i("GPSTracker","Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
-	            Log.i("GPSTracker",e.getMessage());
-	        }
-		}
-		return true;
-	}
-
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when a long press occurs on the trackpad
-	 */
-	@Override
-	public void onLongPress(MotionEvent e) {
-		
-	}
-
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when a slow scroll occurs on the trackpad
-	 */
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
-			float distanceY) {
-		
-		return true;
-	}
-
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when a down MotionEvent occurs and has not 
-	 * moved or released
-	 */
-	@Override
-	public void onShowPress(MotionEvent e) {
-		
-	}
-
-	/**
-	 * This function is for the GestureDetector for Google Glass 
-	 * It is called when the TrackPad is tapped
-	 */
-	@Override
-	public boolean onSingleTapUp(MotionEvent e) {
-		Log.e("GPSTracker", "Tap Function On");
-		 try {
-	            UnityPlayer.UnitySendMessage("Scriptholder", "isTap", "");
-	            Log.i("GPSTracker", "Message Sent: Tap");
-	        } catch (UnsatisfiedLinkError er) {
-	            Log.i("GPSTracker","Failed to send unity message, probably because Unity native libraries aren't available (e.g. you are not running this from Unity");
-	            Log.i("GPSTracker",er.getMessage());
-	        }
-		return true;
-	}
-	  
+	
 }
