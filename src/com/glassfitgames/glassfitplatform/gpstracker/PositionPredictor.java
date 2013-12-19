1c07428b4d4dc537ceea7d7168473c8bfa255aa7
shasha79 <alexander.raginsky@gmail.com>
2013-12-19 01:09:32 +0100

    Tuning bearing algorithm

--- src/com/glassfitgames/glassfitplatform/gpstracker/PositionPredictor.java
+++ src/com/glassfitgames/glassfitplatform/gpstracker/PositionPredictor.java
@@ -171,14 +171,13 @@ public class PositionPredictor {
     	/*System.out.printf("GPS DIST: %f, EST DIST: %f, OFFSET: %f\n" , 
     			gpsTraveledDistance,predictedTraveledDistance, offset);
 		*/
-        double coeff = (offset > 0 ) ? 0.5 : -0.5;        
+        double coeff = (offset > 0 ) ? 0.3 : -0.3;        
         coeff = Math.abs(offset) <= aLastPos.getSpeed() ? offset/aLastPos.getSpeed() : coeff;
 
         double correctedSpeed = aLastPos.getSpeed()*(1 + coeff);
         
         // System.out.printf("SPEED: %f, CORRECTED SPEED: %f, DISTANCE COEFF: %f\n",aLastPos.getSpeed(), correctedSpeed, coeff);
-        return (float) correctedSpeed;
-    	
+        return (float) correctedSpeed;    	
     }
    
     // Correct bearing to adapt slowly to the GPS curve
