d0544bdf115282fdeadd63065e3b8d5b46a4d5fa
Ben Lister <ben.lister@cantab.net>
2014-02-25 10:59:41 +0000

    Replaced dead-reckoning with smoothed distance extrapolation. Not yet tested

--- src/com/glassfitgames/glassfitplatform/gpstracker/StreamedTargetTracker.java
+++ src/com/glassfitgames/glassfitplatform/gpstracker/StreamedTargetTracker.java
@@ -62,20 +62,58 @@ public class StreamedTargetTracker extends TrackTargetTracker {
         return System.currentTimeMillis() - start;
     }
 
+    private double extrapolatedRemoteDistance = 0;  // dead reckoning based on points from remote device
+    private double extrapolatedLocalDistance = 0;  // smooth version used to control avatar
+    private long tickTime = 0;  // time of this invocation
+    private long lastTickTime = 0; // time of last invocation
+    private long lastSampleTime = 0; // time of last remote point
+    private static final long DISTANCE_CORRECTION_MILLISECONDS = 1500;   // time period over which to converge smooth local distance with remote distance
     
+    /**
+     * Extrapolates a smooth distance based on streamed positions from a remote device
+     * Currently assumes all remote positions have timestamps < startOfTrackTime + time
+     */
     @Override
     public double getCumulativeDistanceAtTime(long time) {
-        double distance = super.getCumulativeDistanceAtTime(time);
-        if (super.hasFinished()) {
-            // Simple dead reckoning
-            Position tail = getTail();
-            long recordedTime = tail.getDeviceTimestamp() - getHead().getDeviceTimestamp();
-            long interp = time - recordedTime;
-            if (interp > 5000) interp = 5000; // Lag limit
-            return distance + (tail.getSpeed() * interp / 1000.0);
+        
+        // if we're just starting, set the local & remote distances to the last sample
+        if (lastTickTime == 0) {
+            extrapolatedRemoteDistance = super.getCumulativeDistanceAtTime(time); // distance at last sample
+            extrapolatedLocalDistance = extrapolatedRemoteDistance;
+            tickTime = getTail().getDeviceTimestamp();  // time of last sample
         }
-        return distance;
+        
+        // lastTickTime = time of last invocation, tickTime = time of this invocation
+        lastSampleTime = getTail().getDeviceTimestamp();
+        lastTickTime = tickTime;
+        tickTime = getHead().getDeviceTimestamp() + time;
+        if (tickTime < lastSampleTime) Log.w("StreamedTargetTracker","Remote position arrived with future timestamp");
+        long timeSinceLastSample = tickTime - lastSampleTime;
+        long timeSinceLastInvocation = tickTime - lastTickTime;
+        if (timeSinceLastSample > 5000) {
+            timeSinceLastSample = 5000; // Lag limit, pauses extrapolation of remote distance
+            timeSinceLastInvocation = 0; // Pauses extrapolation of local distance
+        }
+        
+        // calculate the speed we need to move at to make our smooth local
+        // distance converge with the non-continuous remote distance over
+        // a period of DISTANCE_CORRECTION_MILLISECONDS
+        // Will be identical to speed at last sample if this is the first invocation
+        double correctiveSpeed = super.getCurrentSpeed(time)
+                + (extrapolatedRemoteDistance - extrapolatedLocalDistance) * 1000.0
+                / DISTANCE_CORRECTION_MILLISECONDS;
+        
+        // Simple dead reckoning for remote player for next invocation. Non-continuous when a new sample comes in.
+        extrapolatedRemoteDistance = super.getCumulativeDistanceAtTime(time);
+        if (super.hasFinished()) {
+            extrapolatedRemoteDistance += (super.getCurrentSpeed(time) * timeSinceLastSample / 1000.0);
+        }
+        
+        // extrapolate smooth, continuous internal distance for next invocation & return. Always continuous.
+        extrapolatedLocalDistance += correctiveSpeed * timeSinceLastInvocation / 1000.0;
+        return extrapolatedLocalDistance;
     }
+    
 
     @Override
     public boolean hasFinished() {
