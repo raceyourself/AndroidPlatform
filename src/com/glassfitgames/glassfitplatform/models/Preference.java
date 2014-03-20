5e88d2c94494bcb1dd82ef7b4b1c052ef7a70e48
Ben Lister <ben.lister@cantab.net>
2014-03-20 15:30:37 +0000

    Added support for boolean prefs

--- src/com/glassfitgames/glassfitplatform/models/Preference.java
+++ src/com/glassfitgames/glassfitplatform/models/Preference.java
@@ -26,6 +26,11 @@ public class Preference extends Entity {
 	    this.value = ByteBuffer.allocate(8).putLong(value);
 	}
 	
+	public Preference(String key, Boolean value) {
+        this.key = key;
+        this.value = ByteBuffer.allocate(8).putInt(value.booleanValue() ? 1 : 0);
+    }
+	
 	public static Long getLong(String key) {
 	    Preference p = query(Preference.class).where(Query.eql("key", key)).execute();
 	    if (p != null) {
@@ -35,10 +40,31 @@ public class Preference extends Entity {
 	    }
 	}
 	
+	public static Boolean getBoolean(String key) {
+        Preference p = query(Preference.class).where(Query.eql("key", key)).execute();
+        if (p != null) {
+            return (p.value.getInt() == 1 ? true : false);
+        } else {
+            return null;
+        }
+    }
+	
     public static boolean setLong(String key, Long value) {
         Preference p = query(Preference.class).where(Query.eql("key", key)).execute();
         if (p != null) {
             p.value = ByteBuffer.allocate(8).putLong(value);
+            p.save();
+            return true;
+        } else {
+            new Preference(key, value).save();
+            return true;
+        }
+    }
+    
+    public static boolean setBoolean(String key, Boolean value) {
+        Preference p = query(Preference.class).where(Query.eql("key", key)).execute();
+        if (p != null) {
+            p.value = ByteBuffer.allocate(8).putInt(value.booleanValue() ? 1 : 0);
             p.save();
             return true;
         } else {
