3b687d191f55f8c9819c3b409ae087ee2534cea4
Janne Husberg <janne@husberg.fi>
2014-06-30 18:13:57 +0100

    New production server

--- platform/src/main/java/com/raceyourself/platform/utils/Utils.java
+++ platform/src/main/java/com/raceyourself/platform/utils/Utils.java
@@ -16,7 +16,7 @@ public class Utils {
     // Tail skip count (records synced from tail pivot backwards)
     public static final String SYNC_TAIL_SKIP = "last_synced_tail_skip"; 
 
-    public static final String WS_URL = "http://auth.raceyourself.com/";
+    public static final String WS_URL = "https://api.raceyourself.com/";
 //    public static final String WS_URL = "http://a.staging.raceyourself.com/";
     public static final String API_URL = WS_URL + "api/1/";
 
@@ -25,8 +25,8 @@ public class Utils {
 
     // Web service oauth credentials
     // Production:
-    public static final String CLIENT_ID = "8c8f56a8f119a2074be04c247c3d35ebed42ab0dcc653eb4387cff97722bb968";
-    public static final String CLIENT_SECRET = "892977fbc0d31799dfc52e2d59b3cba88b18a8e0080da79a025e1a06f56aa8b2";
+    public static final String CLIENT_ID = "98f985cd4fca00aefda3f31c10b3d994eaa496d882fdf3db936fad76e4dae236";
+    public static final String CLIENT_SECRET = "9ca4b4f56b518deca0c2200b92a3435f05cb4e0b3d52b0a5a1608f39d004750e";
     // Staging:
 //    public static final String CLIENT_ID = "293a42e2e4e2caeff758bbe512059365002d2c6524c1f17c57154f332941e9cd";
 //    public static final String CLIENT_SECRET = "fb47367332bac7633b9cb6f12b411327a1a8df257852c61feac75ff9d0311c53";
