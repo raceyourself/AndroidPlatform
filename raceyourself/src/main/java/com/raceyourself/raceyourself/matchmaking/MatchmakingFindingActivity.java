c6c162fe74bf432d43e8d38f91781f5bc701d12c
amerigo14 <amerigo14@hotmail.com>
2014-07-02 12:02:24 +0100

    Almost complete finding opponent screen - needs profile pictures and opponent name

--- raceyourself/src/main/java/com/raceyourself/raceyourself/matchmaking/MatchmakingFindingActivity.java
+++ raceyourself/src/main/java/com/raceyourself/raceyourself/matchmaking/MatchmakingFindingActivity.java
@@ -1,36 +1,148 @@
 package com.raceyourself.raceyourself.matchmaking;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.raceyourself.raceyourself.R;
 
 public class MatchmakingFindingActivity extends Activity {
 
-    TextView matching;
-    Animation anim;
+    TextView matchingText;
+    TextView searchingText;
+    TextView matrixText;
+    TextView foundText;
+
+    ImageView heartIcon;
+    ImageView globeIcon;
+    ImageView wandIcon;
+    ImageView tickIcon;
+
+    Animation translateRightAnim;
+    Animation rotationAnim;
+
+    Drawable heartIconDrawable;
+    Drawable globeIconDrawable;
+    Drawable wandIconDrawable;
+    Drawable tickIconDrawable;
+
+    Drawable spinnerIconDrawable;
+
+    Button raceButton;
+
+    int animationCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_matchmaking_finding);
-        matching = (TextView)findViewById(R.id.matchingText);
-        anim = AnimationUtils.loadAnimation(this, R.anim.matched_text_anim);
 
-        matching.setOnClickListener(new View.OnClickListener() {
+        matchingText = (TextView)findViewById(R.id.matchingText);
+        searchingText = (TextView)findViewById(R.id.searchingText);
+        matrixText = (TextView)findViewById(R.id.matrixText);
+        foundText = (TextView)findViewById(R.id.matchedText);
+
+        heartIcon = (ImageView)findViewById(R.id.heartIcon);
+        globeIcon = (ImageView)findViewById(R.id.globeIcon);
+        wandIcon = (ImageView)findViewById(R.id.wandIcon);
+        tickIcon = (ImageView)findViewById(R.id.tickIcon);
+
+        translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.matched_text_anim);
+        rotationAnim = AnimationUtils.loadAnimation(this, R.anim.rotating_icon_anim);
+
+        heartIconDrawable = getResources().getDrawable(R.drawable.ic_heart_grey);
+        globeIconDrawable = getResources().getDrawable(R.drawable.ic_globe_grey);
+        wandIconDrawable = getResources().getDrawable(R.drawable.ic_wand_grey);
+        tickIconDrawable = getResources().getDrawable(R.drawable.ic_tick_grey);
+        spinnerIconDrawable = getResources().getDrawable(R.drawable.ic_spinner);
+
+        raceButton = (Button)findViewById(R.id.startRaceBtn);
+
+        translateRightAnim.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View v) {
-                matching.startAnimation(anim);
+            public void onAnimationStart(Animation animation) {
+
             }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                switch(animationCount) {
+                    case 0:
+                        startImageAnimation(heartIcon);
+                        break;
+
+                    case 1:
+                        startImageAnimation(globeIcon);
+                        break;
+
+                    case 2:
+                        startImageAnimation(wandIcon);
+                        break;
+
+                    case 3:
+                        startImageAnimation(tickIcon);
+                        break;
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
         });
+
+        rotationAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                switch(animationCount) {
+                    case 0:
+                        endImageAnimation(heartIcon, heartIconDrawable, searchingText);
+                        break;
+                    case 1:
+                        endImageAnimation(globeIcon, globeIconDrawable, matrixText);
+                        break;
+                    case 2:
+                        endImageAnimation(wandIcon, wandIconDrawable, foundText);
+                        break;
+                    case 3:
+                        tickIcon.setImageDrawable(tickIconDrawable);
+                        raceButton.setVisibility(View.VISIBLE);
+                        break;
+                }
+                if(animationCount < 3) {
+                    animationCount++;
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+        matchingText.startAnimation(translateRightAnim);
     }
 
+
+    public void startImageAnimation(ImageView imageView) {
+        imageView.setImageDrawable(spinnerIconDrawable);
+        imageView.setVisibility(View.VISIBLE);
+        imageView.startAnimation(rotationAnim);
+    }
+
+    public void endImageAnimation(ImageView imageView, Drawable drawable, TextView textView) {
+        imageView.setImageDrawable(drawable);
+        textView.setVisibility(View.VISIBLE);
+        textView.startAnimation(translateRightAnim);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
