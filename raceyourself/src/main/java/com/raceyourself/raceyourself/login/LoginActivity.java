dc7085c2421404bfd9b384d64965e8853337714c
Duncan Roberts <dliroberts@gmail.com>
2014-07-02 14:51:21 +0100

    logging debug statements now working via slf4j

--- raceyourself/src/main/java/com/raceyourself/raceyourself/login/LoginActivity.java
+++ raceyourself/src/main/java/com/raceyourself/raceyourself/login/LoginActivity.java
@@ -143,6 +143,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor>{
      * errors are presented and no actual login attempt is made.
      */
     public void attemptLogin() {
+
         if (mAuthTask != null) {
             return;
         }
