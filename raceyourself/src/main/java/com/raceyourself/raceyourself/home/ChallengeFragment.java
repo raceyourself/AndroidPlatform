8dcfce922d041bb3eb66882ba57d6acbfa428f57
amerigo14 <amerigo14@hotmail.com>
2014-07-08 13:42:20 +0100

    Made challenges an expandable list

--- raceyourself/src/main/java/com/raceyourself/raceyourself/home/ChallengeFragment.java
+++ raceyourself/src/main/java/com/raceyourself/raceyourself/home/ChallengeFragment.java
@@ -1,6 +1,7 @@
 package com.raceyourself.raceyourself.home;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.app.ListFragment;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -36,7 +38,7 @@ import lombok.extern.slf4j.Slf4j;
  * interface.
  */
 @Slf4j
-public class ChallengeFragment extends ListFragment implements AbsListView.OnItemClickListener {
+public class ChallengeFragment extends Fragment {
 
     private OnFragmentInteractionListener listener;
 
@@ -51,15 +53,26 @@ public class ChallengeFragment extends ListFragment implements AbsListView.OnIte
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setListAdapter(new ChallengeListAdapter(getActivity(),
-                android.R.layout.simple_list_item_1, ChallengeNotificationBean.from(Notification.getNotificationsbyType("challenge"))));
+//        setListAdapter(new ChallengeListAdapter(getActivity(),
+//                android.R.layout.simple_list_item_1, ChallengeNotificationBean.from(Notification.getNotificationsbyType("challenge"))));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_challenge_list, container, false);
+
+        ExpandableListView listView = (ExpandableListView)view.findViewById(R.id.challengeList);
+
+        listView.setAdapter(new ChallengeExpandableListAdapter(getActivity(), ChallengeNotificationBean.from(Notification.getNotificationsbyType("challenge"))));
+
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        getListView().setOnItemClickListener(this);
     }
 
     @Override
@@ -74,12 +87,12 @@ public class ChallengeFragment extends ListFragment implements AbsListView.OnIte
         listener = null;
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (listener != null) {
-            listener.onFragmentInteraction((ChallengeNotificationBean)getListAdapter().getItem(position));
-        }
-    }
+//    @Override
+//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//        if (listener != null) {
+//            listener.onFragmentInteraction((ChallengeNotificationBean)getListAdapter().getItem(position));
+//        }
+//    }
 
     /**
     * This interface must be implemented by activities that contain this
@@ -108,39 +121,39 @@ public class ChallengeFragment extends ListFragment implements AbsListView.OnIte
             this.context = context;
         }
 
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-            if (view == null) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                view = inflater.inflate(R.layout.fragment_challenge_notification, null);
-            }
-
-            ChallengeNotificationBean notif = (ChallengeNotificationBean)getListAdapter().getItem(position);
-            DurationChallengeBean chal = (DurationChallengeBean) notif.getChallenge(); // TODO avoid cast - more generic methods in ChallengeBean? 'limit' and 'goal'?
-
-            TextView itemView = (TextView) view.findViewById(R.id.challenge_notification_challenger_name);
-            itemView.setText(notif.getUser().getName());
-
-//            TextView distanceView = (TextView) view.findViewById(R.id.challenge_notification_distance);
-//            String distanceText = getString(R.string.challenge_notification_distance);
-//            double miles = metresToMiles.convert(chal.getDistanceMetres());
-//            distanceView.setText(String.format(distanceText, chal.getDistanceMetres(), DISTANCE_LABEL));
-
-            TextView durationView = (TextView) view.findViewById(R.id.challenge_notification_duration);
-            String durationText = getString(R.string.challenge_notification_duration);
-            int duration = chal.getDuration().toStandardMinutes().getMinutes();
-            log.debug("Duration text and value: {} / {}", durationText, duration);
-            durationView.setText(String.format(durationText, duration));
-
-            TextView expiryView = (TextView) view.findViewById(R.id.challenge_notification_expiry);
-            expiryView.setText(PeriodFormat.getDefault().print(new Period(new DateTime(), new DateTime(notif.getExpiry()))));
-
-            TextView subtitleView = (TextView) view.findViewById(R.id.challenge_notification_challenge_subtitle);
-            if (notif.isFromMe()) subtitleView.setText(R.string.challenge_sent);
-            else subtitleView.setText(R.string.challenge_received);
-
-            return view;
-        }
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            View view = convertView;
+//            if (view == null) {
+//                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//                view = inflater.inflate(R.layout.fragment_challenge_notification, null);
+//            }
+//
+//            ChallengeNotificationBean notif = (ChallengeNotificationBean)getListAdapter().getItem(position);
+//            DurationChallengeBean chal = (DurationChallengeBean) notif.getChallenge(); // TODO avoid cast - more generic methods in ChallengeBean? 'limit' and 'goal'?
+//
+//            TextView itemView = (TextView) view.findViewById(R.id.challenge_notification_challenger_name);
+//            itemView.setText(notif.getUser().getName());
+//
+////            TextView distanceView = (TextView) view.findViewById(R.id.challenge_notification_distance);
+////            String distanceText = getString(R.string.challenge_notification_distance);
+////            double miles = metresToMiles.convert(chal.getDistanceMetres());
+////            distanceView.setText(String.format(distanceText, chal.getDistanceMetres(), DISTANCE_LABEL));
+//
+//            TextView durationView = (TextView) view.findViewById(R.id.challenge_notification_duration);
+//            String durationText = getString(R.string.challenge_notification_duration);
+//            int duration = chal.getDuration().toStandardMinutes().getMinutes();
+//            log.debug("Duration text and value: {} / {}", durationText, duration);
+//            durationView.setText(String.format(durationText, duration));
+//
+//            TextView expiryView = (TextView) view.findViewById(R.id.challenge_notification_expiry);
+//            expiryView.setText(PeriodFormat.getDefault().print(new Period(new DateTime(), new DateTime(notif.getExpiry()))));
+//
+//            TextView subtitleView = (TextView) view.findViewById(R.id.challenge_notification_challenge_subtitle);
+//            if (notif.isFromMe()) subtitleView.setText(R.string.challenge_sent);
+//            else subtitleView.setText(R.string.challenge_received);
+//
+//            return view;
+//        }
     }
 }
 
