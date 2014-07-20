eebabea42908b106a286b7fa1324522b73176dbc
Ben Lister <ben.lister@cantab.net>
2014-07-20 09:36:42 +0100

    Removed unused/unfound reference to GpsPopup

--- raceyourself/src/main/java/com/raceyourself/raceyourself/game/GameActivity.java
+++ raceyourself/src/main/java/com/raceyourself/raceyourself/game/GameActivity.java
@@ -34,7 +34,6 @@ import com.raceyourself.raceyourself.base.util.PictureUtils;
 import com.raceyourself.raceyourself.game.event_listeners.GameEventListener;
 import com.raceyourself.raceyourself.game.event_listeners.RegularUpdateListener;
 import com.raceyourself.raceyourself.game.popup.GpsOverlay;
-import com.raceyourself.raceyourself.game.popup.GpsPopup;
 import com.raceyourself.raceyourself.game.popup.PauseOverlay;
 import com.raceyourself.raceyourself.game.popup.QuitOverlay;
 import com.raceyourself.raceyourself.game.position_controllers.FixedVelocityPositionController;
